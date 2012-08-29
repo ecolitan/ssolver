62f4b0d948ca5439a1d81d7bbdcd52b99c76dde8
Aaron Cossey <aaron.cossey@gmail.com>
2012-08-29 14:52:49 +0200

    add tests

---
+++ testcases.py
@@ -0,0 +1,16 @@
+from ssolver import SSolver
+
+testposition = [[5,3,None,None,7,None,None,None,None],
+              [6,None,None,1,9,5,None,None,None],
+              [None,9,8,None,None,None,None,6,None],
+              [8,None,None,None,6,None,None,None,3],
+              [4,None,None,8,None,3,None,None,1],
+              [7,None,None,None,2,None,None,None,6],
+              [None,6,None,None,None,None,2,8,None],
+              [None,None,None,4,1,9,None,None,5],
+              [None,None,None,None,8,None,None,7,9]]
+testObj = SSolver()
+testObj.UpdatePosition(testposition)
+testObj.UpdateFieldPossibilies()
+testObj.SearchHiddenSingles()
+testObj.IsSolved()
